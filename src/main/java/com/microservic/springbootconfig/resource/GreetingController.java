ee3d8885fce0f64200a1adec58cc1dd792eb9aa6
Chinmay Bhat <chinmay.bhat@cerner.com>
2020-04-12 01:26:24 +0530

    Configure spring boot using @Value annotation

--- src/main/java/com/microservic/springbootconfig/resource/GreetingController.java
+++ src/main/java/com/microservic/springbootconfig/resource/GreetingController.java
@@ -1,5 +1,8 @@
 package com.microservic.springbootconfig.resource;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	@Value("${my.greeting}")
+	@Value("${my.greeting: default value}")
 	private String greetingMessage;
+
+	@Value("This is a static value")
+	private String someStaticValue;
+
+	@Value("${my.list.greeting}")
+	private List<String> myList;
+
+	@Value("#{${my.details}}")
+	private Map<String, String> myDetails;
 
 	@GetMapping("/greeting")
 	public String greeting() {
-		return greetingMessage;
+		return new StringBuilder(greetingMessage)
+				.append(" ").append(someStaticValue)
+				.append("").append(myList)
+				.append(" ").append(myDetails.get("lastname"))
+				.append(",").append(myDetails.get("firstname"))
+				.toString();
 	}
 }
